a4d7526c20aad4847747b363f85bd5e5bb7b879c
elizhang227 <zhang.eli@hotmail.com>
2019-06-03 14:54:20 -0400

    first commit for topics app

---
+++ routes/topics.js
@@ -0,0 +1,68 @@
+const express = require('express'),
+    router = express.Router(),
+    TopicsModel = require('../models/topics');
+
+router.get('/', async (req, res, next) => {
+    const allTopics = await TopicsModel.getAllTopics();
+
+    res.render('template', { 
+        locals: {
+            title: 'List of Topics from Class',
+            topicList: allTopics
+        },
+        partials : {
+            content: 'partial-topics'
+        }
+    });
+})
+
+// router.post('/', (req, res) => {
+//     const { name, ranking } = req.body;
+
+//     TopicsModel.addTopic(name, ranking)
+//     .then(async () => {
+//         const allTopics = await TopicsModel.getAllTopics();
+//         await TopicsModel.updateTopic();
+
+//         //testtest();
+
+//         res.status(200).render('template', {
+//             locals: {
+//                 title: 'List of Topics from Class',
+//                 topicList: allTopics
+//             },
+//             partials: {
+//                 content: 'partial-topics'
+//             }
+//         });
+//     })
+//     .catch((err) => {
+//         res.sendStatus(500).send(err.message);
+//     });
+// });
+
+router.post('/', (req, res) => {
+    const { name, ranking } = req.body;
+
+    TopicsModel.updateTopic(name, ranking)
+    .then(async () => {
+        const allTopics = await TopicsModel.getAllTopics();
+
+        //testtest();
+
+        res.status(200).render('template', {
+            locals: {
+                title: 'List of Topics from Class',
+                topicList: allTopics
+            },
+            partials: {
+                content: 'partial-topics'
+            }
+        });
+    })
+    .catch((err) => {
+        res.sendStatus(500).send(err.message);
+    });
+});
+
+module.exports = router;
